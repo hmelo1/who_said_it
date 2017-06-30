7a852e99bef687eabd8511d418938202011b49a0
Unknown <joelbitar@protonmail.com>
2017-06-29 21:48:46 -0400

    Abstracted seed data
    Created json import method

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,143 +1,41 @@
-# Donald Trump
+def json_import(filename, name, title, img)
+	character = Character.create(
+		name: name,
+		title: title,
+		img: img
+	)
 
-donald_trump = Character.create(
-	name: "Donald Trump", 
-	title: "45th U.S. President, Vocal Misogynist", 
-	img: "donald_trump.png"
-)
-
-donald_trump_json = ActiveSupport::JSON.decode(File.read('db/seeds/quotes/donald_trump.json'))
-
-donald_trump_json.each do |key, value|
-  value.each do |quote|
-  	Quote.create(content: quote["content"], source: quote["source"], tag: quote["tag"], character_id: donald_trump.id)
-  end
+	json = ActiveSupport::JSON.decode(File.read("db/seeds/quotes/#{filename}.json"))
+	json.each do |key, value|
+	  value.each do |quote|
+	  	Quote.create(content: quote["content"], source: quote["source"], tag: quote["tag"], character_id: character.id)
+	  end
+	end
 end
+
+# Donald Trump
+json_import("donald_trump", "Donald Trump", "45th U.S. President, Vocal Misogynist", "donald_trump.png")
 
 # George W. Bush
-
-george_w_bush = Character.create(
-	name: "George W. Bush", 
-	title: "43rd U.S. President, Dodger of Shoes", 
-	img: "george_w_bush.png"
-)
-
-george_w_bush_json = ActiveSupport::JSON.decode(File.read('db/seeds/quotes/george_w_bush.json'))
-
-george_w_bush_json.each do |key, value|
-  value.each do |quote|
-  	Quote.create(content: quote["content"], source: quote["source"], tag: quote["tag"], character_id: george_w_bush.id)
-  end
-end
+json_import("george_w_bush", "George W. Bush", "43rd U.S. President, Dodger of Shoes", "george_w_bush.png")
 
 # Hitler
-
-hitler = Character.create(
-	name: "Adolf Hitler", 
-	title: "Leader of the Nazi Party, Mass-Murderer", 
-	img: "hitler.png"
-)
-
-hitler_json = ActiveSupport::JSON.decode(File.read('db/seeds/quotes/hitler.json'))
-
-hitler_json.each do |key, value|
-  value.each do |quote|
-  	Quote.create(content: quote["content"], source: quote["source"], tag: quote["tag"], character_id: hitler.id)
-  end
-end
+json_import("hitler", "Adolf Hitler", "Leader of the Nazi Party, Mass-Murderer", "hitler.png")
 
 # Mr. Krabs
-
-mr_krabs = Character.create(
-	name: "Eugene Krabs", 
-	title: "Owner of the Krusty Krab, Avid Capitalist", 
-	img: "mr_krabs.png"
-)
-
-mr_krabs_json = ActiveSupport::JSON.decode(File.read('db/seeds/quotes/mr_krabs.json'))
-
-mr_krabs_json.each do |key, value|
-  value.each do |quote|
-  	Quote.create(content: quote["content"], source: quote["source"], tag: quote["tag"], character_id: mr_krabs.id)
-  end
-end
+json_import("mr_krabs", "Eugene Krabs", "Owner of the Krusty Krab, Avid Capitalist", "mr_krabs.png")
 
 # Skeletor
-
-skeletor = Character.create(
-	name: "skeletor", 
-	title: "Owner of the Krusty Krab, Avid Capitalist", 
-	img: "skeletor.png"
-)
-
-skeletor_json = ActiveSupport::JSON.decode(File.read('db/seeds/quotes/skeletor.json'))
-
-skeletor_json.each do |key, value|
-  value.each do |quote|
-  	Quote.create(content: quote["content"], source: quote["source"], tag: quote["tag"], character_id: skeletor.id)
-  end
-end
+json_import("skeletor", "Skeletor", "Aspiring Master of the Universe, Archenemy of He-Man", "skeletor.png")
 
 # Stalin
+json_import("joseph_stalin", "Joseph Stalin", "Soviet Revolutionary, Red Terror Extraordinaire", "stalin.png")
 
-stalin = Character.create(
-	name: "Joseph Stalin", 
-	title: "Soviet Revolutionary, Red Terror Enthusiast", 
-	img: "stalin.png"
-)
-
-stalin_json = ActiveSupport::JSON.decode(File.read('db/seeds/quotes/stalin.json'))
-
-stalin_json.each do |key, value|
-  value.each do |quote|
-  	Quote.create(content: quote["content"], source: quote["source"], tag: quote["tag"], character_id: stalin.id)
-  end
-end
-
-# Stewie 
-
-stewie = Character.create(
-	name: "Stewie Griffin", 
-	title: "Baby, Matricidal Enthusiast", 
-	img: "stewie.png"
-)
-
-stewie_json = ActiveSupport::JSON.decode(File.read('db/seeds/quotes/stewie.json'))
-
-stewie_json.each do |key, value|
-  value.each do |quote|
-  	Quote.create(content: quote["content"], source: quote["source"], tag: quote["tag"], character_id: stewie.id)
-  end
-end
+# Stewie
+json_import("stewie", "Stewie Griffin", "Baby, Deranged Matricidal Maniac", "stewie.png")
 
 # The Brain
-
-the_brain = Character.create(
-	name: "Stewie Griffin", 
-	title: "Baby, Matricidal Enthusiast", 
-	img: "the_brain.png"
-)
-
-the_brain_json = ActiveSupport::JSON.decode(File.read('db/seeds/quotes/the_brain.json'))
-
-the_brain_json.each do |key, value|
-  value.each do |quote|
-  	Quote.create(content: quote["content"], source: quote["source"], tag: quote["tag"], character_id: the_brain.id)
-  end
-end
+json_import("the_brain", "The Brain", "Lab Mouse Obsessed with World Domination", "the_brain.png")
 
 # Ursula
-
-ursula = Character.create(
-	name: "Ursula", 
-	title: "Villainous Sea Witch", 
-	img: "ursula.png"
-)
-
-ursula_json = ActiveSupport::JSON.decode(File.read('db/seeds/quotes/ursula.json'))
-
-ursula_json.each do |key, value|
-  value.each do |quote|
-  	Quote.create(content: quote["content"], source: quote["source"], tag: quote["tag"], character_id: ursula.id)
-  end
-end
+json_import("ursula", "Ursula", "Villainous Sea Witch", "ursula.png")
