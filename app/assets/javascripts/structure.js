c3970c8eee943124bc95465a36b69c6cba90cce2
joel <joelbitar@protonmail.com>
2017-07-07 18:49:59 -0400

    character selection form almost ready for ajax

--- app/assets/javascripts/structure.js
+++ app/assets/javascripts/structure.js
@@ -3,7 +3,7 @@ $(function() {
 	$character_select_div = $('#character-select');
 
 	// Hide Character Selection
-	// $character_select_div.hide().removeClass('hide');
+	$character_select_div.hide().removeClass('hide');
     
   // Play Game (Click to Start Game) -- Section #1  
 	$('#play-game').click(function() {
