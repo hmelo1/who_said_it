9b276782ab7f5340d4b061966a6334c3d5995bb0
joel <joelbitar@protonmail.com>
2017-07-07 10:36:39 -0400

    organized external stylesheets/sped up title animation

--- app/assets/javascripts/animations.js
+++ app/assets/javascripts/animations.js
@@ -1,11 +1,7 @@
 $(function() {
     
+	// Title Div
 	var subtitle = $('#subtitle');
-
-	// Simple Tween
-	//TweenLite.from(img, 1, {x: -200, ease: RoughEase.ease.config({ template: Power0.easeNone, strength: 1, points: 20, taper: "none", randomize: true, clamp: true})});
-	//TweenLite.from(img, 1, {x: -200, ease: SlowMo.ease.config(0.7, 0.7, false)});
-	TweenLite.from(subtitle, 1, {autoAlpha: 0, y: 30, delay: 5, ease: Power0.easeNone});
-	//TweenLite.from(h2, 1, {autoAlpha: 0, delay: 1});
+	TweenLite.from(subtitle, 1, {autoAlpha: 0, y: 10, delay: 3.5, ease: Power0.easeNone});
 
 });
