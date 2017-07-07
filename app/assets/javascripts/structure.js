577fb6e1e8752f7bea8d154684e86e0650b3726e
joel <joelbitar@protonmail.com>
2017-07-07 15:27:51 -0400

    began implementing card selection

--- app/assets/javascripts/structure.js
+++ app/assets/javascripts/structure.js
@@ -3,12 +3,17 @@ $(function() {
 	$character_select_div = $('#character-select')
 
 	// Hide Character Selection
-	$character_select_div.hide().removeClass('hide');
+	// $character_select_div.hide().removeClass('hide');
     
   // Play Game (Click to Start Game) -- Section #1  
 	$('#play-game').click(function() {
 		$welcome_div.fadeOut();
 		$character_select_div.delay(400).fadeIn(300);
+	})
+
+	// Character Selection
+	$('.card').click(function() {
+		alert('hello')
 	})
 
 });
