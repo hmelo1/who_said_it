ed3b61261b372ce9267142ada3f39c8f4105b3dd
joel <joelbitar@protonmail.com>
2017-07-07 14:18:04 -0400

    transition to character selection

--- app/assets/javascripts/animations.js
+++
@@ -1,7 +0,0 @@
-// $(function() {
-    
-// 	// Title Div
-// 	var subtitle = $('#subtitle');
-// 	TweenLite.from(subtitle, 1, {autoAlpha: 0, y: 10, delay: 3.5, ease: Power0.easeNone});
-
-// });
---
+++ app/assets/javascripts/structure.js
@@ -0,0 +1,14 @@
+$(function() {
+	$welcome_div = $('#welcome')
+	$character_select_div = $('#character-select')
+
+	// Hide Character Selection
+	$character_select_div.hide().removeClass('hide');
+    
+  // Play Game (Click to Start Game) -- Section #1  
+	$('#play-game').click(function() {
+		$welcome_div.fadeOut();
+		$character_select_div.delay(400).fadeIn(300);
+	})
+
+});
