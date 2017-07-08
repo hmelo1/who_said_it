a1614ab88e20ef117cdc6d53a78fcb8d0e7aa12d
joel <joelbitar@protonmail.com>
2017-07-08 14:17:18 -0400

    merged skeleton css into project

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,5 @@
 class HomeController < ApplicationController
 	def index
 		@characters = Character.all
-		@donald_trump = Character.find_by(name: "Donald Trump")
-		@george_w_bush = Character.find_by(name: "George W. Bush")
-		@hitler = Character.find_by(name: "Adolf Hitler")
-		@mr_krabs = Character.find_by(name: "Eugene Krabs")
-		@skeletor = Character.find_by(name: "Skeletor")
-		@stewie = Character.find_by(name: "Stewie Griffin")
-		@joseph_stalin = Character.find_by(name: "Joseph Stalin")
-		@bill_hicks = Character.find_by(name: "Bill Hicks")
-		@george_carlin = Character.find_by(name: "George Carlin")
 	end
 end
