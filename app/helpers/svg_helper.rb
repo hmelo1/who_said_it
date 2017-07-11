edff3948a2b29acbcc167399aeb79782f8f11e8c
joel <joelbitar@protonmail.com>
2017-07-11 10:01:38 -0400

    renamed SVG helper

--- app/helpers/svg_helper.rb
+++ app/helpers/svg_helper.rb
@@ -1,4 +1,4 @@
-module IndexHelper
+module SvgHelper
 	def svg(name)
 	  file_path = "#{Rails.root}/app/assets/images/#{name}.svg"
 	  return File.read(file_path).html_safe if File.exists?(file_path)
