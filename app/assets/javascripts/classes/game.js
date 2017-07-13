3ea68b8d95dc1a35c9ab05d405e3ce35ab08b86a
joel <joelbitar@protonmail.com>
2017-07-13 09:09:32 -0400

    refactor jquery

--- app/assets/javascripts/classes/game.js
+++ app/assets/javascripts/classes/game.js
@@ -7,13 +7,3 @@ class Game {
 	  this.completed = attributes.completed;
 	}
 }
-
-function shuffleArray(array) {
-	for (var i = array.length - 1; i > 0; i--) {
-  	var j = Math.floor(Math.random() * (i + 1));
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-  return array;
-}
--- app/assets/javascripts/structure/interface.js
+++ app/assets/javascripts/structure/interface.js
@@ -1,63 +1,63 @@
-$(function() {
-	$welcome_div = $('#welcome');
-	$character_select_div = $('#character-select');
+// $(function() {
+// 	$welcome_div = $('#welcome');
+// 	$character_select_div = $('#character-select');
 
-	// Hide Character Selection
-	// $character_select_div.hide().removeClass('hide');
+// 	// Hide Character Selection
+// 	// $character_select_div.hide().removeClass('hide');
 
-  // Play Game (Click to Start Game) -- Section #1
-	$('#play-game').click(function() {
-		$welcome_div.fadeOut();
-		$character_select_div.delay(400).fadeIn(300);
-	})
+//   // Play Game (Click to Start Game) -- Section #1
+// 	$('#play-game').click(function() {
+// 		$welcome_div.fadeOut();
+// 		$character_select_div.delay(400).fadeIn(300);
+// 	})
 
-	// Character Selection -- Section #2
+// 	// Character Selection -- Section #2
 
-	// Create a 'Selected/Active State for Clicked Cards
-	$('.card').click(function() {
-		if ($('.selected').length < 2) {
-			$(this).toggleClass('selected');
-		} else {
-			$(this).removeClass('selected');
-		}
-	})
+// 	// Create a 'Selected/Active State for Clicked Cards
+// 	$('.card').click(function() {
+// 		if ($('.selected').length < 2) {
+// 			$(this).toggleClass('selected');
+// 		} else {
+// 			$(this).removeClass('selected');
+// 		}
+// 	})
 
-	// AJAX Post to create new game
-	$('#create-game').submit(function(event) {
-		event.preventDefault();
+// 	// AJAX Post to create new game
+// 	$('#create-game').submit(function(event) {
+// 		event.preventDefault();
 
-		if ($('.selected').length < 2) {
-			alert('Please select 2 characters');
-		} else {
-			var AUTH_TOKEN = $("input[name='authenticity_token']").val();
-			var url = this.action
-			 // + "?&authenticity_token=" + AUTH_TOKEN
-			var character1 = $('.selected :input')[0];
-			var character2 = $('.selected :input')[1];
+// 		if ($('.selected').length < 2) {
+// 			alert('Please select 2 characters');
+// 		} else {
+// 			var AUTH_TOKEN = $("input[name='authenticity_token']").val();
+// 			var url = this.action
+// 			 // + "?&authenticity_token=" + AUTH_TOKEN
+// 			var character1 = $('.selected :input')[0];
+// 			var character2 = $('.selected :input')[1];
 
-			data = {
-				'authenticity_token': AUTH_TOKEN,
-				'characters' : [
-				{
-					'name': character1.name,
-					'id': character1.id
-				},
-				{
-					'name': character2.name,
-					'id': character2.id
-				}]
-			}
+// 			data = {
+// 				'authenticity_token': AUTH_TOKEN,
+// 				'characters' : [
+// 				{
+// 					'name': character1.name,
+// 					'id': character1.id
+// 				},
+// 				{
+// 					'name': character2.name,
+// 					'id': character2.id
+// 				}]
+// 			}
 
-			$.ajax({
-				type: "POST",
-				url: url,
-				data: data,
-				success: function(response) {
-					debugger;
-				}
-			});
-		}
-	})
+// 			$.ajax({
+// 				type: "POST",
+// 				url: url,
+// 				data: data,
+// 				success: function(response) {
+// 					debugger;
+// 				}
+// 			});
+// 		}
+// 	})
 
 
-});
+// });
