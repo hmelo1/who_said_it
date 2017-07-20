7ab80d7db2796a0919fb9da613e385fc5f98968d
Unknown <joelbitar@protonmail.com>
2017-07-20 13:17:31 -0400

    Stable Implementation of Browserify

--- app/assets/javascripts/modules/main/gameModule.js
+++ app/assets/javascripts/modules/main/gameModule.js
@@ -3,8 +3,8 @@
   var gameModule = {
   	// Game data passed from character screen
     createGame: function(data) {
+      var game = new Game(data);
       debugger
-      var game = new Game(data);  
     }
   }
 
