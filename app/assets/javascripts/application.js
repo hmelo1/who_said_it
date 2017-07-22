792bd30fa90344917f3f89cd1840d8303afb79be
joel <joelbitar@protonmail.com>
2017-07-21 23:39:09 -0400

    added flipping css cards

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,7 +7,10 @@
 
 //= require classes/game
 
+// Components
 require('./modules/components/navModule');
+
+// Modules
 require('./modules/structure/welcomeModule');
 require('./modules/structure/characterModule');
 require('./modules/structure/gameModule');
