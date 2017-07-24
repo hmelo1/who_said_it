28b930878ef3eed0e9ec0fd957eb987dc815e4d2
joel <joelbitar@protonmail.com>
2017-07-23 20:26:30 -0400

    mechanism to generate one quote at a time

--- app/assets/javascripts/modules/structure/gameModule.js
+++ app/assets/javascripts/modules/structure/gameModule.js
@@ -4,12 +4,21 @@
   	// Game data passed from character screen
     createGame: function(data) {
       var game = new Game(data);
+		  
 		  var pictureTemplate = Handlebars.compile($('#game-picture-template').html());
 		  var characters = pictureTemplate(game.characters);
 		  $('#game-pictures').html(characters);
-		  var quoteTemplate = Handlebars.compile($('#game-quote-template').html());
-		  var quotes = quoteTemplate(game.quotes);
-		  $('#game-quotes').html(quotes);
+		  
+		  var counter = 0;
+		  generateQuote(game.quotes[counter]);
+
+
+		  function generateQuote(gameQuote) {
+		  	var quoteTemplate = Handlebars.compile($('#game-quote-template').html());
+			  var quote = quoteTemplate(gameQuote);
+			  $('#game-quotes').html(quote);
+		  }
+		  
   	}
   }
 
