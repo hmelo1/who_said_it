ec7e643550013d4058987f250b0c6e924cf9b2af
joel <joelbitar@protonmail.com>
2017-07-25 13:51:49 -0400

    feature: game stops after submitting last answer

--- app/assets/javascripts/modules/structure/gameModule.js
+++ app/assets/javascripts/modules/structure/gameModule.js
@@ -13,34 +13,34 @@
 
       $('.game-character-submit').on("click", function(){
         analyzeQuote(this);
-        newQuote();
         checkGameOver();
       });
+
+      function checkGameOver() {
+        counter++;
+        console.log(counter)
+        if (counter >= 10) {
+          debugger
+        } else {
+          newQuote();
+        }
+      }
       
       function analyzeQuote(originalThis) {
-        if (counter < 10) {
-          if ($(originalThis).data('id') == game.quotes[counter].character_id) {
-            alert('correct');
-            game.state.push(true);
-          } else {
-            alert('incorrect');
-            game.state.push(false);
-          }
-          counter++;
+        if ($(originalThis).data('id') == game.quotes[counter].character_id) {
+          alert('correct');
+          game.state.push(true);
+        } else {
+          alert('incorrect');
+          game.state.push(false);
         }
       } 
 
       function newQuote() {
-        if (counter < 10) {
-          var quoteTemplate = Handlebars.compile($('#game-quote-template').html());
-          var quote = quoteTemplate(game.quotes[counter]);
-          $('#game-quotes').html(quote);
-        }
+        var quoteTemplate = Handlebars.compile($('#game-quote-template').html());
+        var quote = quoteTemplate(game.quotes[counter]);
+        $('#game-quotes').html(quote);
       } 
-
-      function checkGameOver() {
-        eval('debugger');
-      }
 
       function firstQuote(gameQuote) {
         var quoteTemplate = Handlebars.compile($('#game-quote-template').html());
