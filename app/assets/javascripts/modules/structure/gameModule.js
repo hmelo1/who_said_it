642c29a646e8c8670bd5be76c1e8875af6ba918f
joel <joelbitar@protonmail.com>
2017-07-24 21:00:45 -0400

    ten quotes pushed into state

--- app/assets/javascripts/modules/structure/gameModule.js
+++ app/assets/javascripts/modules/structure/gameModule.js
@@ -13,27 +13,30 @@
 
       $('.game-character-submit').on("click", function(){
         analyzeQuote(this);
-        counter++;
         newQuote();
       });
       
       function analyzeQuote(originalThis) {
-        if ($(originalThis).data('id') == game.quotes[counter].character_id) {
-          alert('correct');
-          game.state.push(true);
+        if (counter <= 9 ) {
+          if ($(originalThis).data('id') == game.quotes[counter].character_id) {
+            alert('correct');
+            game.state.push(true);
+          } else {
+            alert('incorrect');
+            game.state.push(false);
+          }
+          counter++;
         } else {
-          alert('incorrect');
-          game.state.push(false);
+          debugger
         }
       }
+          
 
       function newQuote() {
-        if (counter < 9) {
+        if (counter <= 9) {
           var quoteTemplate = Handlebars.compile($('#game-quote-template').html());
           var quote = quoteTemplate(game.quotes[counter]);
           $('#game-quotes').html(quote);
-        } else {
-          alert('No More Quotes Available');
         }
       } 
 
