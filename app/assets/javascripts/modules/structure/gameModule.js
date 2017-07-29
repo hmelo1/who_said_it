fc1d4eaca9f24d982421a67660b5037a65585f05
joel <joelbitar@protonmail.com>
2017-07-28 20:19:43 -0400

    Comments: added comments to game module

--- app/assets/javascripts/modules/structure/gameModule.js
+++ app/assets/javascripts/modules/structure/gameModule.js
@@ -29,15 +29,22 @@
       this.playGame();
     },
     playGame: function() {
+      // Game Functions
+
+      // Start Counter at 0
       this.counter = 0;
+      // Render Characters and First Quote
       this.renderCharacters(this.game.characters);
       this.renderQuote();
       // Items need to be cached after handlebars templates are rendered
       this.cacheHandlebars();
+      // New Quotes Render onClick of GameCards
       this.bindEvents();
+      // One Bind Event for Modal Close (and check if game is completed)
       this.bindModalEvents();
     },
     checkAnswer: function(originalEvent) {
+      // Variables to Check Character ID/NAME of Clicked Card and Character Who Said Quote
       var clickedCardId = $(originalEvent.target).parents('.gamecard').data('id');
       var clickedCardName = $(originalEvent.target).parents('.gamecard').data('name');
       var answerCharacterId = this.game.quotes[this.counter].character_id;
@@ -53,41 +60,53 @@
         this.game.state.push(false);
       }
     },
+    // This function Should Run After Modal is Escaped
     checkComplete: function() {
       this.counter++;
 
       if (this.counter < 10) {
-        // Continue Game
+        // If Game Continues -- Render Quote
         this.renderQuote();
       } else {
-        // End Game
+        // Else -- End Game and Save to Database
         this.saveCompletedGame();
       }
     },
     renderQuote: function() {
+      // Render Handlebars Quote Template
       var quoteTemplate = Handlebars.compile($('#game-quote-template').html());
       var quote = quoteTemplate(this.game.quotes[this.counter]);
       this.$gameQuotes.html(quote);
     },
     renderCharacters: function(gameCharacters) {
+      // Get the URL of fingerprinted asset on users computer (Regex removes Base URL)
       var image1path = $(`img[alt='${this.game.characters[0].name}']`).prop('src').replace(/^(?:\/\/|[^\/]+)*\//, "");
       var image2path = $(`img[alt='${this.game.characters[1].name}']`).prop('src').replace(/^(?:\/\/|[^\/]+)*\//, "");
+
+      // Render Handlebars Characters Template
       var pictureTemplate = Handlebars.compile($('#game-picture-template').html());
       var characters = pictureTemplate(gameCharacters);
       this.$gamePictures.html(characters);
+
+      // Insert Images Into Character Template
       $('#selected-character-0').attr('src', image1path);
       $('#selected-character-1').attr('src', image2path);
     },
     saveCompletedGame: function() {
+      // Mark non-database game as complete
       this.game.completed = true;
-      var originalThis = this;
-      var url = "/games/save";
 
+      // Declare and Assign Module for AJAX
+      var originalThis = this;
+
+      // AJAX Params
+      var url = "/games/save";
       var game_data = {
         id: this.game.id,
         state: this.game.state
       }
 
+      // AJAX Post to Save Game in Database
       $.ajax({
         type: "POST",
         url: url,
@@ -97,15 +116,19 @@
         }
       });
     },
+    // Modal Functions
     bindModalEvents: function() {
+      // Click Anywhere On Screen To Close Modal
       this.$modal.on('click', this.hideModal.bind(this))
     },
     showModal: function(evaluate, correctAnswer, userAnswer) {
-      var originalThis = this;
+      // Pass in 'Correct/Incorrect' into Modal Before Displaying It
       this.$modalEvaluate.text(evaluate);
+      // Pass in Correct Answer into Modal Before Displaying It
       this.$modalCorrectAnswer.text("Correct Answer: " + correctAnswer);
+      // Pass in User Answer into Modal Before Displaying It
       this.$modalUserAnswer.text("Your Answer:  " + userAnswer);
-
+      // Display Modal
       this.$modal.css('display', 'block');
     },
     hideModal: function() {
