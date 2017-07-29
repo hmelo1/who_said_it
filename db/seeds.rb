4f6baf178d193a6d475982038ca3300c23157742
joel <joelbitar@protonmail.com>
2017-07-29 15:59:18 -0400

    Tested Chomsky vs Kissinger

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,8 @@ def character_import(filename, name, description, img, img_selected)
 	end
 end
 
+# Adolf Hitler
+character_import("adolf_hitler", "Adolf Hitler", "Leader of the Nazi Party, mass-murderer and most famous white supremacist in world history.", "adolf_hitler.jpg", "adolf_hitler_selected.jpg")
 
 # Bill Hicks
 character_import("bill_hicks", "Bill Hicks", "Stand-up comedian, chain smoker and raging misanthropist.", "bill_hicks.jpg", "bill_hicks_selected.jpg")
@@ -33,20 +35,17 @@ character_import("george_carlin", "George Carlin", "Stand-up Comedian, renowned 
 # God
 character_import("god", "God", "Allegedly created the universe in 6 days. Known to get into fits of jealous rage.", "god.jpg", "god_selected.jpg")
 
-# Hitler
-character_import("hitler", "Adolf Hitler", "Leader of the Nazi Party, mass-murderer and most famous white supremacist in world history.", "hitler.jpg", "hitler_selected.jpg")
-
-# King Joffrey
-character_import("mr_burns", "Mr. Burns", "Nuclear Power Plant Owner, trap door activator and ruthless businessman.", "mr_burns.jpg", "mr_burns_selected.jpg")
-
 # Henry Kissinger
 character_import("henry_kissinger", "Henry Kissinger", "56th Secretary of State, Committed Genocide with a Swagger", "henry_kissinger.jpg", "henry_kissinger_selected.jpg",)
+
+# Mr. Burns
+character_import("mr_burns", "Mr. Burns", "Nuclear Power Plant Owner, trap door activator and ruthless businessman.", "mr_burns.jpg", "mr_burns_selected.jpg")
 
 # Noam Chomsky
 character_import("noam_chomsky", "Noam Chomsky", "Social-critic, World Renowned Intellectual", "noam_chomsky.jpg", "noam_chomsky_selected.jpg")
 
-# Stalin
+# Joseph Stalin
 character_import("joseph_stalin", "Joseph Stalin", "Ex-Soviet Leader and prize-winning political repressor.", "stalin.jpg", "stalin_selected.jpg")
 
-# Stewie
+# Stewie Griffin
 character_import("stewie_griffin", "Stewie Griffin", "Deranged matricidal infant with a cuddly side.", "stewie_griffin.jpg", "stewie_griffin_selected.jpg")
